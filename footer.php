dec5cf5e2b7ac56bb52185bbd29b9633b5020bc5
IagoCampideli <campidellisup@gmail.com>
2021-11-05 16:13:56 -0300

    Salvando em repositorio o site feito no curso da Origamid de WordPress

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer>
+			<nav class="nav-footer">
+			<?php
+				$args = array(
+					'menu' => 'principal',
+					'container' => false
+				);
+			wp_nav_menu( $args );
+			?>
+			</nav>
+
+			<p>Rest © 2015. Alguns direitos reservados.</p>
+		</footer>
+            <?php wp_footer(); ?>
+	</body>
+</html>
